fab2249b3b2090fd3523a9af7df044327e72ac72
lpsohail07@gmail.com <lpsohail07@gmail.com>
2025-07-09 08:19:35 +0000

    tests with macro

---
+++ macros/mac_no_null_check.sql
@@ -0,0 +1,9 @@
+-- singular test macro
+
+{% macro mac_no_null_check(model) %}
+    SELECT * FROM {{ model }} WHERE
+    {% for col in adapter.get_columns_in_relation(model) -%}
+        {{ col.column }} IS NULL OR
+    {% endfor %}
+    FALSE
+{% endmacro %}
